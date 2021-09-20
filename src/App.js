9aed2714c62ea7576717221254aee555f1112fe5
GabrielJuliao <gabrielbjuliao@gmail.com>
2021-09-19 23:58:19 -0300

    fake api fix

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import TopBar from "./components/TobBar";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [videos, setVideos] = useState([
-    {
-      thumbnail: "https://picsum.photos/310/170?random=1",
-      avatar: "https://picsum.photos/36/36?random=2",
-      duration: "1:30",
-      title: "Sample Video Title",
-      channelName: "Channel Name",
-      metadata: "8 mi views • 1 day ago",
-    },
-  ]);
+  const [videos, setVideos] = useState([]);
 
   useEffect(()=> {
     fetch('/mock-api.json')
